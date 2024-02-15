99e2a8f49d606e4731a81dece375c65a4d006a22
Paul <awolesiboluwatife@gmail.com>
2024-02-15 14:31:13 +0100

    fixed button issue

--- src/Hero/HeroSection.jsx
+++ src/Hero/HeroSection.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Navbar';
 import heroImage1 from '../assets/heroImage-1.svg';
 import heroImage2 from '../assets/heroImage-2.svg';
 import heroImage3 from '../assets/heroImage-3.svg';
 
 function HeroSection() {
+	const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsDesktop(window.innerWidth >= 768);
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
 	return (
 		<div className="hero">
 			<Navbar />
 			<div className="main-hero-section max-w-5xl flex flex-col gap-y-2 items-center justify-center mx-auto py-14 px-10 md:px-10 xl:px-0">
 				<div className="hero-content lg:px-5 flex flex-col gap-y-5">
-					<p className="tag mx-auto text-white font-semibold text-sm">
+					<p className="tag mx-auto text-white text-center font-semibold text-sm">
 						Meet the best product designers in Poland
 					</p>
 					<h1 className="text-4xl text-black md:text-5xl xl:text-[60px] leading-[1.2] md:max-w-xl mx-auto text-center lg:max-w-3xl font-bold">
@@ -30,13 +43,18 @@ function HeroSection() {
 							/>
 						</div>
 						<button className="bg-[#304EE6] text-white w-1/3 hover:shadow-lg border border-transparent hover:drop-shadow transition duration-200 lg:w-1/3 font-semibold text-xs lg:text-base rounded-lg px-5 py-3">
-							Try it out
+							{isDesktop ? 'Try it out' : 'Send'}
 						</button>
 					</div>
 				</div>
 			</div>
 			<div className="max-w-lg mx-auto w-full flex items-center justify-center px-8 lg:px-4 lg:justify-start gap-x-1 lg:gap-x-8">
-				<img src="/multi.png" width="50%" alt="" className="hidden lg:flex" />
+				<img
+					src="/multi.png"
+					width="50%"
+					alt=""
+					className="hidden lg:flex"
+				/>
 				<p>
 					<span className="font-semibold">More than 100+</span> users
 					are selling their products online with simple & easy ways
